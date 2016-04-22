009b6fb8a6b59560ab3c20b6a2f3184fee7765c1
Alexander Lombardo <alexanderjlombardo@gmail.com>
2016-04-22 16:28:54 -0700

    Tic-tac-toe starting point

---
+++ starter-code/TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
@@ -0,0 +1,21 @@
+package ly.generalassemb.drewmahrt.tictactoe;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+
+    int i;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        for (i = 0; i<9; i++) {
+
+        }
+    }
+
+
+}
---
+++ starter-code/TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
@@ -0,0 +1,46 @@
+package ly.generalassemb.drewmahrt.tictactoe;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+//    String playerOne;
+//    String playerTwo;
+//    ArrayList<String> players;
+//    ArrayAdapter<String> mPlayers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    protected void startPlaying(View view) {
+
+        EditText playerOne = (EditText)findViewById(R.id.player_one_name);
+        EditText playerTwo = (EditText)findViewById(R.id.player_two_name);
+
+        if(playerOne.toString().isEmpty() || playerTwo.toString().isEmpty()){
+            Toast.makeText(MainActivity.this, "Who is playing!?", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("SharedKey", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString("playerOneName", playerOne.getText().toString());
+        editor.putString("playerTwoName", playerTwo.getText().toString());
+
+        editor.commit();
+
+        Intent startGame = new Intent(MainActivity.this, GameActivity.class);
+        startActivity(startGame);
+    }
+}
