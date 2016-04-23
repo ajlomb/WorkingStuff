34b13c932b9ea91d6bed8c17ebd7b024a8f43b6f
Alexander Lombardo <alexanderjlombardo@gmail.com>
2016-04-22 17:25:12 -0700

    game is playable, but no data is being exchanged.

--- starter-code/TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
+++ starter-code/TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
@@ -8,20 +8,27 @@ import android.widget.TextView;
 public class GameActivity extends AppCompatActivity {
 
     double counter;
-    TextView boxOne;
-    TextView boxTwo;
+    TextView playerTurn;
+    TextView boxOne, boxTwo, boxThree, boxFour, boxFive, boxSix, boxSeven, boxEight, boxNine;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        playerTurn = (TextView)findViewById(R.id.game_message_text);
         boxOne = (TextView)findViewById(R.id.textView);
         boxTwo = (TextView)findViewById(R.id.textView2);
+        boxThree = (TextView)findViewById(R.id.textView3);
+        boxFour = (TextView)findViewById(R.id.textView4);
+        boxFive = (TextView)findViewById(R.id.textView5);
+        boxSix = (TextView)findViewById(R.id.textView6);
+        boxSeven = (TextView)findViewById(R.id.textView7);
+        boxEight = (TextView)findViewById(R.id.textView8);
+        boxNine = (TextView)findViewById(R.id.textView9);
 
         double counter = Math.random() * 2;                                               // Sets random player as first player
-
-        TextView playerTurn = (TextView) findViewById(R.id.game_message_text);
 
         if (counter % 2 == 0) {
 
@@ -39,6 +46,68 @@ public class GameActivity extends AppCompatActivity {
         } else if (counter % 2 == 1) {
             boxOne.setText("o");
         }
+        counter++;
+    }
+    public void TV2(View view) {
+        if (counter % 2 == 0) {
+            boxTwo.setText("x");
+        } else if (counter % 2 == 1) {
+            boxTwo.setText("o");
+        }
+        counter++;
+    }
+    public void TV3(View view) {
+        if (counter % 2 == 0) {
+            boxThree.setText("x");
+        } else if (counter % 2 == 1) {
+            boxThree.setText("o");
+        }
+        counter++;
+    }
+    public void TV4(View view) {
+        if (counter % 2 == 0) {
+            boxFour.setText("x");
+        } else if (counter % 2 == 1) {
+            boxFour.setText("o");
+        }
+        counter++;
+    }
+    public void TV5(View view) {
+        if (counter % 2 == 0) {
+            boxFive.setText("x");
+        } else if (counter % 2 == 1) {
+            boxFive.setText("o");
+        }
+        counter++;
+    }
+    public void TV6(View view) {
+        if (counter % 2 == 0) {
+            boxSix.setText("x");
+        } else if (counter % 2 == 1) {
+            boxSix.setText("o");
+        }
+        counter++;
+    }
+    public void TV7(View view) {
+        if (counter % 2 == 0) {
+            boxSeven.setText("x");
+        } else if (counter % 2 == 1) {
+            boxSeven.setText("o");
+        }counter++;
+    }
+    public void TV8(View view) {
+        if (counter % 2 == 0) {
+            boxEight.setText("x");
+        } else if (counter % 2 == 1) {
+            boxEight.setText("o");
+        }counter++;
+    }
+    public void TV9(View view) {
+        if (counter % 2 == 0) {
+            boxNine.setText("x");
+        } else if (counter % 2 == 1) {
+            boxNine.setText("o");
+        }counter++;
     }
 }
 
