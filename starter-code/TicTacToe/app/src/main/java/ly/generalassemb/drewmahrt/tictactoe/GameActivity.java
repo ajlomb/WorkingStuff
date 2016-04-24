ec3728c6b5220a09d4e1bd12869c8cd0b4d8ac49
Alexander Lombardo <alexanderjlombardo@gmail.com>
2016-04-24 13:11:45 -0700

    I am going about this all wrong

--- starter-code/TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
+++ starter-code/TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
@@ -12,6 +12,8 @@ public class GameActivity extends AppCompatActivity {
     String playerOneName1, playerTwoName2;
     TextView playerTurn;
     TextView boxOne, boxTwo, boxThree, boxFour, boxFive, boxSix, boxSeven, boxEight, boxNine;
+//    ArrayList<Integer> p1, p2;
+  //  ArrayAdapter<String> mPlayerAdapter;
 
 
     @Override
@@ -29,34 +31,50 @@ public class GameActivity extends AppCompatActivity {
         boxSeven = (TextView) findViewById(R.id.textView7);
         boxEight = (TextView) findViewById(R.id.textView8);
         boxNine = (TextView) findViewById(R.id.textView9);
+//        p1 = new ArrayList<>();
+//        p2 = new ArrayList<>();
 
         SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("SharedKey", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
         sharedPreferences.getString("playerOneName", "");
         sharedPreferences.getString("playerTwoName", "");
 
         playerOneName1 = String.valueOf(sharedPreferences.getString("playerOneName", ""));
         playerTwoName2 = String.valueOf(sharedPreferences.getString("playerTwoName", ""));
 
-  //      counter = 2;
+        playerTurn.setText(playerOneName1);
 
-
-            if (counter % 2 == 0) {
-                playerTurn.setText(playerOneName1);
-            } else if (counter % 2 == 1) {
-                playerTurn.setText(playerTwoName2);
-            }
-
+        if ((boxOne.equals(":(") && boxTwo.equals(":(") && boxThree.equals(":(")) ||
+                (boxFour.equals(":(") && boxFive.equals(":(") && boxSix.equals(":(")) ||
+                (boxSeven.equals(":(") && boxEight.equals(":(") && boxNine.equals(":(")) ||
+                (boxOne.equals(":(") && boxFive.equals(":(") && boxNine.equals(":(")) ||
+                (boxThree.equals(":(") && boxFive.equals(":(") && boxSeven.equals(":("))) {
+            editor.putString("winnerWinner", playerOneName1);
+            editor.commit();
+        } else if ((boxOne.equals(":)") && boxTwo.equals(":)") && boxThree.equals(":)")) ||
+                (boxFour.equals(":)") && boxFive.equals(":)") && boxSix.equals(":)")) ||
+                (boxSeven.equals(":)") && boxEight.equals(":)") && boxNine.equals(":)")) ||
+                (boxOne.equals(":)") && boxFive.equals(":)") && boxNine.equals(":)")) ||
+                (boxThree.equals(":)") && boxFive.equals(":)") && boxSeven.equals(":)"))) {
+            editor.putString("winnerWinner", playerTwoName2);
+            editor.commit();
+        }
 
     }
+
+
+
 
 
     public void TV1(View view) {
         if (counter % 2 == 0) {
             boxOne.setText(":(");
             playerTurn.setText(playerTwoName2);
+//            p1.add(1);
         } else if (counter % 2 == 1) {
             boxOne.setText(":)");
             playerTurn.setText(playerOneName1);
+ //           p2.add(1);
         }view.setClickable(false);
         counter++;
     }
@@ -64,9 +82,11 @@ public class GameActivity extends AppCompatActivity {
         if (counter % 2 == 0) {
             boxTwo.setText(":(");
             playerTurn.setText(playerTwoName2);
+ //           p1.add("2");
         } else if (counter % 2 == 1) {
             boxTwo.setText(":)");
             playerTurn.setText(playerOneName1);
+//            p2.add("2");
         }view.setClickable(false);
         counter++;
     }
@@ -74,9 +94,11 @@ public class GameActivity extends AppCompatActivity {
         if (counter % 2 == 0) {
             boxThree.setText(":(");
             playerTurn.setText(playerTwoName2);
+//            p1.add("3");
         } else if (counter % 2 == 1) {
             boxThree.setText(":)");
             playerTurn.setText(playerOneName1);
+ //           p2.add("3");
         }view.setClickable(false);
         counter++;
     }
@@ -84,9 +106,11 @@ public class GameActivity extends AppCompatActivity {
         if (counter % 2 == 0) {
             boxFour.setText(":(");
             playerTurn.setText(playerTwoName2);
+ //           p1.add("4");
         } else if (counter % 2 == 1) {
             boxFour.setText(":)");
             playerTurn.setText(playerOneName1);
+//            p2.add("4");
         }view.setClickable(false);
         counter++;
     }
@@ -94,9 +118,11 @@ public class GameActivity extends AppCompatActivity {
         if (counter % 2 == 0) {
             boxFive.setText(":(");
             playerTurn.setText(playerTwoName2);
+  //          p1.add("5");
         } else if (counter % 2 == 1) {
             boxFive.setText(":)");
             playerTurn.setText(playerOneName1);
+   //         p2.add("5");
         }view.setClickable(false);
         counter++;
     }
@@ -104,9 +130,11 @@ public class GameActivity extends AppCompatActivity {
         if (counter % 2 == 0) {
             boxSix.setText(":(");
             playerTurn.setText(playerTwoName2);
+ //           p1.add("6");
         } else if (counter % 2 == 1) {
             boxSix.setText(":)");
             playerTurn.setText(playerOneName1);
+ //           p2.add("6");
         }view.setClickable(false);
         counter++;
     }
@@ -114,9 +142,11 @@ public class GameActivity extends AppCompatActivity {
         if (counter % 2 == 0) {
             boxSeven.setText(":(");
             playerTurn.setText(playerTwoName2);
+   //         p1.add("7");
         } else if (counter % 2 == 1) {
             boxSeven.setText(":)");
             playerTurn.setText(playerOneName1);
+  //          p2.add("7");
         }view.setClickable(false);
         counter++;
     }
@@ -124,9 +154,11 @@ public class GameActivity extends AppCompatActivity {
         if (counter % 2 == 0) {
             boxEight.setText(":(");
             playerTurn.setText(playerTwoName2);
+ //           p1.add("8");
         } else if (counter % 2 == 1) {
             boxEight.setText(":)");
             playerTurn.setText(playerOneName1);
+ //           p2.add("8");
         }view.setClickable(false);
         counter++;
     }
@@ -134,9 +166,11 @@ public class GameActivity extends AppCompatActivity {
         if (counter % 2 == 0) {
             boxNine.setText(":(");
             playerTurn.setText(playerTwoName2);
+  //          p1.add("9");
         } else if (counter % 2 == 1) {
             boxNine.setText(":)");
             playerTurn.setText(playerOneName1);
+  //          p2.add("9");
         }view.setClickable(false);
         counter++;
     }
